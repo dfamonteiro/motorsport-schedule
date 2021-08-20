254522dc8b2d13752e4df1935aacb15ceee9b9bf
Daniel Monteiro <daniel.amaro.monteiro@gmail.com>
2021-08-20 13:24:39 +0100

    Found package that works

--- parse_F1.py
+++ parse_F1.py
@@ -1,7 +1,7 @@
-from ics import Calendar
+from icalendar import Calendar
 import requests
 
-url = "https://www.formula1.com/calendar/Formula_1_Official_Calendar.ics"
-c = Calendar(requests.get(url).text)
+url = "http://www.formula1.com/calendar/Formula_1_Official_Calendar.ics"
+c = Calendar.from_ical(requests.get(url).text)
 
 print(c)
